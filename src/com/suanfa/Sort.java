8be46d16ec1f4d6099ebec086764bf3a83b9a7a6
Lin Yimin <mealie_lin@163.com>
2018-09-04 11:25:26 +0800

    排序算法

--- src/com/suanfa/Sort.java
+++ src/com/suanfa/Sort.java
@@ -12,9 +12,6 @@ public class Sort {
         Integer[] in = {2, 8, 1, 5, 10, 9, 0};
 
         efferArray(in);
-        printMulti();
-
-        printNumAssemble(10);
     }
 
     /**
@@ -78,31 +75,4 @@ public class Sort {
         System.out.println("移动次数：" + upnum);
         System.out.print("\n\n");
     }
-
-    /**打印乘法口诀*/
-    private static void printMulti() {
-        System.out.println("----打印乘法口诀-----");
-        for (int j = 1; j < 10; j++) {
-            for (int i = 1; i <= j; i++) {
-                System.out.print(i + " * " + j + " = " + j * i + "\t");
-            }
-            System.out.print("\t\n");
-        }
-        System.out.print("\n\n");
-    }
-
-    /**打印N * 1 + N * 2 + N * 3 =num的所有组合*/
-    private static void printNumAssemble(int num) {
-        for (int i = 0; i < num + 1; i++) {
-            for (int j = 0; j < num / 2 + 1; j++) {
-                for (int in = 0; in < num / 3 + 1; in ++) {
-                    if (i * 1 + j * 2 + in * 3 == num) {
-                        System.out.println("小马" + i + ",\t中马" + j + ",\t大马" + in );
-                    }
-                }
-            }
-        }
-    }
-
-
 }
